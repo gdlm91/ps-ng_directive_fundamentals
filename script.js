eaf1622cdd1f76912b4a34500b63c535da7d83b7
Gerardo Leal <gerard.leal91@gmail.com>
2016-12-07 17:45:32 -0300

    First Directive

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 (function() {
     'use strict';
 
-    angular.module('app', [
-        
-    ]);
+    angular.module('app', [])
+    
+    .controller('mainCtrl', function($scope) {
+        $scope.user = {
+            name: 'Luke Skywalker',
+            address: {
+                street: 'PO Box 123',
+                city: 'Secret Rebel Base',
+                planet: 'Yavin 4'
+            },
+            friends: ['Han', 'Leia', 'Chewbacca']
+        }
+
+    })
+
+    .directive('userInfoCard', function() {
+        return {
+            templateUrl: "userInfoCard.html",
+            restrinc: 'E'
+        }
+    })
+
 })();
