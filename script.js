22f1e66dfb45a655b255ca09e04869ba2b5e39e9
Gerardo Leal <gerard.leal91@gmail.com>
2016-12-12 10:54:26 -0300

    Events, Scope and Controllers

--- script.js
+++ script.js
@@ -4,7 +4,8 @@
     angular.module('app', [])
     
     .controller('mainCtrl', function($scope) {
-        $scope.user = {
+        
+        $scope.user1 = {
             name: 'Luke Skywalker',
             address: {
                 street: 'PO Box 123',
@@ -14,12 +15,84 @@
             friends: ['Han', 'Leia', 'Chewbacca']
         }
 
+        $scope.user2 = {
+            name: 'Han Solo',
+            address: {
+                street: 'PO Box 123',
+                city: 'Mos Eisley',
+                planet: 'Tattoine'
+            },
+            friends: ['Luke', 'Leia', 'Chewbacca']
+        }
+
     })
 
     .directive('userInfoCard', function() {
         return {
-            templateUrl: "userInfoCard.html",
-            restrinc: 'E'
+            templateUrl: 'userInfoCard.html',
+            restrinc: 'E',
+            scope: {
+                user: '=',
+                initialCollapsed: '@?collapsed'
+            },
+            controller: function($scope) {
+                $scope.collapsed = ($scope.initialCollapsed === 'true');
+
+                $scope.knightUser = function(user) {
+                    user.rank = 'Knight';
+                }
+
+                $scope.collapse = function() {
+                    $scope.collapsed = !$scope.collapsed;
+                }
+
+                $scope.removeFriend = function(friend) {
+                    var idx = $scope.user.friends.indexOf(friend);
+                    if(idx > -1) {
+                        $scope.user.friends.splice(idx, 1);
+                    }
+                }
+            }
+        }
+    })
+
+    .directive('removeFriend', function() {
+        return {
+            restrinc: 'E',
+            templateUrl: 'removeFriend.html',
+            scope: {
+                notifyParent: '&method'
+            },
+            controller: function($scope) {
+                $scope.removing = false;
+
+                $scope.startRemove = function() {
+                    $scope.removing = true;
+                }
+
+                $scope.cancelRemove = function() {
+                    $scope.removing = false;
+                }
+
+                $scope.confirmRemove = function() {
+                    $scope.notifyParent();
+                }
+            }
+        }
+    })
+
+    .directive('address', function() {
+        return {
+            templateUrl: 'address.html',
+            restrinc: 'E',
+            scope: true,
+            controller: function($scope) {
+                $scope.collapsed = false;
+
+                $scope.collapse = function() {
+                    $scope.collapsed = !$scope.collapsed;
+                }
+            }
         }
     })
 
